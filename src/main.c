e95c2efa29c63a5b26e8d52547832243a3a636df
matmatEN57 <matmatEN57@DESKTOP-19NSLJV>
2024-06-30 01:21:31 +0200

    done

--- src/main.c
+++ src/main.c
@@ -3,7 +3,9 @@
 unsigned short int *const LCD_FB = (unsigned short int*) 0xD0000000;
 unsigned short int *const LCD_BUF = (unsigned short int*) 0xD0100000;
 
-unsigned short int menustat=01;
+unsigned short int menustat=1;
+unsigned short int t=0;
+unsigned short int direct=1;
 unsigned short int PozycjePionoweWpisowMenu[] = {20, 50, 80, 150};
 uint8_t text0[]="Oswietlenie";
 uint8_t text1[]="Ogrzewanie";
@@ -12,37 +14,21 @@ uint8_t text3[]="O aplikacji";
 uint8_t* texty[]={text0,text1,text2,text3};
 
 
-// Deklaracja zmiennej ExtraDelay
-unsigned int ExtraDelay;
-enum AppState_t {A_MenuGlowne, A_AnimPodswietleniaMenu, A_AktywacjaWyboruMenu, A_Oswietlenie, A_Ogrzewanie, A_Alarm, A_oAplikacji};
-enum AppState_t AppState = A_MenuGlowne;
-enum PozycjeMenu_t {M_Oswietlenie=0, M_Ogrzewanie=1, M_Alarm=2, M_oAplikacji=3};
 
 
 
 
 
-enum PozycjeMenu_t KolejnePozycjeMenu[] = {M_Ogrzewanie, M_Alarm, M_oAplikacji, M_oAplikacji};
-enum PozycjeMenu_t PoprzedniePozycjeMenu[] = {M_Oswietlenie, M_Oswietlenie, M_Ogrzewanie, M_Alarm};
-enum PozycjeMenu_t AktualnaPozycjaMenu = M_Alarm;
-enum PozycjeMenu_t KolejnaPozycjaMenu;
-
-unsigned int MenuActivationTimer;
-unsigned int MenuAnimationTimer;
-
-// Prototypy funkcji
-
-
-void WypiszMenu(void);
-void RysujPodswietlenie(unsigned int pozycja);
 
 
 
 void WypiszMenu(void) {
+	Clear_And_Reload_Screen();
 	for (unsigned int i=0;i<4;i++){
-		if(1==1) { RysujPodswietlenie(PozycjePionoweWpisowMenu[i]); }
+		if( menustat == i ) { RysujPodswietlenie(PozycjePionoweWpisowMenu[i]); }
 		BSP_LCD_DisplayStringAt(0, PozycjePionoweWpisowMenu[i], texty[i], 1);
 	}
+	t=0;
 }
 
 
@@ -71,96 +57,48 @@ void RysujPodswietlenie(unsigned int pozycja) {
 }
 
 
-
+unsigned short int  nextMenu( unsigned short int current ){
+	switch( current ){
+		case 0: { direct=1; } break;
+		case 3: { direct=-1; } break;
+	}
+	return current+direct;
+}
 
 
 int main(void) {
 System_Init();
-while(1){
-	WypiszMenu();
-	Clear_And_Reload_Screen();
-}
-
-
-
-
-while (1) {
-switch(AppState) {
-case A_MenuGlowne:
-RysujPodswietlenie(PozycjePionoweWpisowMenu[AktualnaPozycjaMenu]);
 WypiszMenu();
-
-
-if(1000 > 3000) {
-MenuAnimationTimer = 100;
-AppState = A_AnimPodswietleniaMenu;
-KolejnaPozycjaMenu = KolejnePozycjeMenu[AktualnaPozycjaMenu];
-}
-if(1000 < 1000) {
-MenuAnimationTimer = 100;
-AppState = A_AnimPodswietleniaMenu;
-KolejnaPozycjaMenu = PoprzedniePozycjeMenu[AktualnaPozycjaMenu];
-}
-break;
-case A_AnimPodswietleniaMenu:
-MenuAnimationTimer--;
-// Animacja paska podświetlania
-unsigned int y0=PozycjePionoweWpisowMenu[AktualnaPozycjaMenu];
-unsigned int y1=PozycjePionoweWpisowMenu[KolejnaPozycjaMenu];
-unsigned int opoznienie_animacji = 100; // Domyślna wartość
-unsigned int y = (y0 * MenuAnimationTimer + y1 * (opoznienie_animacji - MenuAnimationTimer)) / opoznienie_animacji;
-RysujPodswietlenie(y);
-WypiszMenu();
-if(MenuAnimationTimer == 0) {
-AktualnaPozycjaMenu = KolejnaPozycjaMenu;
-AppState = A_MenuGlowne;
-}
-break;
-case A_AktywacjaWyboruMenu:
-	MenuActivationTimer--;
-// Mrugnięcie diodą LED
-	if (MenuActivationTimer == 0) {
-		switch(AktualnaPozycjaMenu) {
-			case M_Oswietlenie:   AppState = A_Oswietlenie;  break;
-			case M_Ogrzewanie:    AppState = A_Ogrzewanie;   break;
-			case M_Alarm:         AppState = A_Alarm;        break;
-			case M_oAplikacji:    AppState = A_oAplikacji;   break;
+t=0;
+	while(1){
+		if ( t>100 ) {
+			t=0;
+			menustat=nextMenu( menustat );
+			WypiszMenu();
 		}
 	}
-break;
-case A_Oswietlenie:
-// Obsługa wyboru oswietlenia w menu, z możliwością powrotu do menu głównego
-break;
-case A_Ogrzewanie:
-// Obsługa wyboru ogrzewania w menu, z możliwością powrotu do menu głównego
-break;
-case A_Alarm:
-// Obsługa wyboru alarmu w menu, z możliwością powrotu do menu głównego
-break;
-case A_oAplikacji:
-// Obsługa wyboru opcji "o aplikacji" w menu, z możliwością powrotu do menu głównego
-break;
 }
-Clear_And_Reload_Screen();
-}
-}
+
 void SysTick_Handler(void) {
-HAL_IncTick();
+	t++;
+	HAL_IncTick();
 }
+
+
+
+
+
+
 void System_Init() {
-HAL_Init();
-SystemClock_Config();
-BSP_SDRAM_Init();
-BSP_LCD_Init();
+	HAL_Init();
+	SystemClock_Config();
+	BSP_SDRAM_Init();
+	BSP_LCD_Init();
 }
+
 void Clear_And_Reload_Screen() {
-for (int off = 0; off < 320*240; off++) {
-LCD_FB[off] = LCD_BUF[off];
-LCD_BUF[off] = 0;
+	for (int off = 0; off < 320*240; off++) {
+		LCD_FB[off] = LCD_BUF[off];
+		LCD_BUF[off] = 0;
+	}
 }
-}
-
-
-
-
-
--- src/main.h
+++ src/main.h
@@ -15,31 +15,13 @@
 
 
 void WypiszMenu(void);
-unsigned int GetJoyPos(void);
+unsigned short int  nextMenu( unsigned short int current );
+
+void RysujPodswietlenie(unsigned int pozycja);
+void SysTick_Handler(void);
+void HAL_IncTick(void);
 
 
-int main2(void) ;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Definicja struktury kwadrat z polami "upakowanymi" i wyrównanymi do 4-ech bajtów
-typedef   struct {
-	unsigned int x, y , size;
-	int  dx , dy;
-} __attribute__((packed,aligned(4))) kwadrat_t ;
-
-#define ABS(x) ((unsigned int)(x>=0?x:-x))
 
 typedef enum
 {
@@ -56,22 +38,10 @@ typedef enum
 } HAL_StatusTypeDef;
 
 void System_Init(void);
-HAL_StatusTypeDef HAL_Init(void);
-void SystemClock_Config(void);
-void handleGPIO(void);
 void Clear_And_Reload_Screen(void);
-void move_square_C(void);
-void drawElements(void );
-void printText(void);
 void BSP_SDRAM_Init(void);
 void BSP_LCD_Init(void);
 void BSP_LCD_DisplayStringAt(uint16_t X, uint16_t Y, uint8_t *pText, Text_AlignModeTypdef mode);
-void BSP_LCD_DisplayRot90StringAt(uint16_t X, uint16_t Y, uint8_t *pText, Text_AlignModeTypdef mode);
 
-void move_square_asm(void);
-void kopiuj_blok_pamieci(unsigned short int* FROM,unsigned short int* INTO,int SIZE);
-void kasuj_blok_pamieci(unsigned short int* WHERE,int SIZE);
-unsigned short int RGB16Pack(unsigned int B,unsigned int G,unsigned int R);
-void fillMemory(void* adres_bazowy, unsigned long rozmiar,unsigned short int wartosc);
-void SysTick_Handler(void);
-void HAL_IncTick(void);
+
+
